e15031739a753797ed53e62313a54edcb0d3cd11
BKC <bkc@NET.local>
2020-07-31 12:09:04 +0530

    first commit

---
+++ src/main/java/com/dhiraj/Controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.dhiraj.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String index() {
+		System.out.println("Index");
+		return "index";
+	}
+
+
+}
